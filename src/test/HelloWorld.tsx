cee37493e19fb8e4fe16fac98fa42551cb38720b
Duong Phan <duongpnh@zigvy.com>
2022-05-31 16:49:02 +0700

    Initial React App with typescript, eslint, webpack

---
+++ src/test/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+function HelloWorld() {
+  return (
+    <h3>Hello World 1asd</h3>
+  );
+}
+
+export default HelloWorld;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      test: path.resolve(process.cwd(), 'src/test/'),
+      // shared: path.resolve(process.cwd(), 'shared/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
